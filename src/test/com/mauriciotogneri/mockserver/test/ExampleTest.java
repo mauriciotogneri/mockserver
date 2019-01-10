efa93d835abf7cece03b8f5e850f3b425472c2b9
Mauricio Togneri <mauricio.togneri@gmail.com>
2019-01-10 14:22:43 +0100

    Making tests with OkHttp

--- src/test/com/mauriciotogneri/mockserver/test/ExampleTest.java
+++ src/test/com/mauriciotogneri/mockserver/test/ExampleTest.java
@@ -1,14 +1,35 @@
 package com.mauriciotogneri.mockserver.test;
 
+import com.mauriciotogneri.mockserver.EndPoint;
+import com.mauriciotogneri.mockserver.MockServer;
+
 import org.junit.Test;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import static org.junit.Assert.assertEquals;
 
 public class ExampleTest
 {
     @Test
-    public void createTable()
+    public void example() throws IOException
     {
-        assertEquals("aaa", "aaa");
+        MockServer mockServer = new MockServer(8080, new ArrayList<EndPoint>());
+        mockServer.start();
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("http://www.google.com")
+                .build();
+
+        Response response = client.newCall(request).execute();
+        String body = response.body().string();
+
+        assertEquals("aaa", body);
     }
 }
